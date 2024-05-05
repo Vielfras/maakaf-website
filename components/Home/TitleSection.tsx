54480c1d2945e3e604639c1179903e71a6702968
vielfras <vielfras@gmail.com>
2024-05-05 23:24:01 +0300

    ltr and rtl in TileSection now changes according to chosen locale

--- components/Home/TitleSection.tsx
+++ components/Home/TitleSection.tsx
@@ -1,21 +1,36 @@
 import DiscordLink from '@/components/Common/DiscordLink';
 import { useTranslations } from 'next-intl';
+import useTypedLocale from '@/hooks/useTypedLocale';
 
 export default function TitleSection() {
   const t = useTranslations('TitleSection');
+  const currentLocale = useTypedLocale();
+  const isEnglish = currentLocale === 'en';
+
   return (
-    <div className="relative mx-auto max-w-[1600px] px-4 flex justify-between h-[90vh] 2xl:h-[90vh] xl:h-[90vh] md:h-[70vh] md:px-14 pb-10 sm:h-[80vh]">
+    <div
+      dir={isEnglish ? 'ltr' : 'rtl'}
+      className={`relative mx-auto max-w-[1600px] px-4 flex justify-between h-[90vh] 2xl:h-[90vh] xl:h-[90vh] md:h-[70vh] md:px-14 pb-10 sm:h-[80vh] ${
+        isEnglish ? 'text-left' : 'text-right'
+      }`}
+    >
       <div className="flex flex-col items-center w-full gap-6 mt-[1.2rem] 2xl:gap-12 lg:px-12 lg:mt-0 lg:gap-8 md:-mt-2 md:px-10 md:w-1/2 md:items-start md:gap-4">
         <h1
           data-testid="homePage-title"
-          className="w-full text-center 2xl:text-5xl xl:text-3xl lg:text-3xl md:text-right md:text-2xl"
+          className={`w-full ${
+            isEnglish ? 'text-left' : 'text-right'
+          } 2xl:text-5xl xl:text-3xl lg:text-3xl md:text-2xl`}
         >
           {t('title')}&nbsp;-
           <br />
           {t('subTitle')}
         </h1>
 
-        <h4 className="w-full text-center 2xl:text-2xl xl:text-sm lg:text-sm md:text-right md:text-1xs">
+        <h4
+          className={`w-full ${
+            isEnglish ? 'text-left' : 'text-right'
+          } 2xl:text-2xl xl:text-sm lg:text-sm md:text-1xs`}
+        >
           {t('firstParagraph')}
           <br /> {t('secondParagraph')}
           <br />
@@ -31,7 +46,6 @@ export default function TitleSection() {
           {t('discordLink')}
         </DiscordLink>
       </div>
-
       <div className="absolute top-0 left-0 w-full h-full opacity-40 -z-10 2xl:h-[90%] xl:w-[650px] md:z-0 md:w-1/2 md:opacity-100 md:static">
         <div
           className={
